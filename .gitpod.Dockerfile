7d3ae6574804d4a8fbb88281a2498c756aaa7008
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-05-31 14:16:44 +0000

    Nodmon

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,6 +1,5 @@
 FROM gitpod/workspace-full
 
-# Install MySQL client and server
-RUN sudo apt-get update && \
-    sudo apt-get install -y mysql-server mysql-client && \
-    sudo rm -rf /var/lib/apt/lists/*
+# Install MySQL Server
+RUN apt-get update \
+    && apt-get install -y mysql-server
--- backend/index.js
+++ backend/index.js
@@ -6,39 +6,28 @@ const app = express();
 app.use(cors());
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',  // or '127.0.0.1'
-  user: 'root',
-  password: '',
-  port: 3306
-});
-
-// Create a connection to the database
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'localhost',  // or '127.0.0.1' or the name of your database container
   user: 'root',
-  password: '',
-  database: 'your_database_name',  // make sure this database exists
+  password: '123456',
   port: 3306
 });
 
-app.get('/api/teams', (req, res) => {
-  connection.query('SELECT * FROM teams', (err, rows) => {
-    if (err) {
-      res.status(500).json({ message: 'Database error' });
-      throw err;
-    }
-    res.json(rows);
-  });
+//Connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySql Connected...');
 });
 
-app.get('/api/odds', (req, res) => {
-  connection.query('SELECT * FROM odds', (err, rows) => {
-    if (err) {
-      res.status(500).json({ message: 'Database error' });
-      throw err;
-    }
-    res.json(rows);
+
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE smash_db';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('database created...');
   });
 });
 
